b5b7a99ef1cb7d2c6809fd515ee9cfeb55226dee
akshadam2118 <88976370+akshadam2118@users.noreply.github.com>
2021-09-03 14:30:21 +0530

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+x = 5
+
+y = 7
+
+z1 = x + y
+
+z2 = x - y
+
+z3 = x*y
+
+z4 = x/y
+
+z5 = x + 2*y
+
+z6 = 3*x + 2*y
+
+z7 = x**2 * y**2
+
+
+
+
+
+
