f603c314e0e28ff0074354c083a5abd7a39e6585
akshadam2118 <88976370+akshadam2118@users.noreply.github.com>
2021-09-03 14:14:13 +0530

    Add files via upload

---
+++ a.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 15 15:42:22 2021
+
+@author: ak
+"""
+
+V = [1,2,3,4,5]
+print(V)
+
+import matplotlib.pyplot as plt
+plt.plot(V)
+
+
+
+List1 = [1, "hello", "world", True]
+
+List2 = list(range(10))
+
+ print(List1)
+ 
+ print(List2)
+ 
+ print(List1[0])
+ 
+ print(List2[2:])
+ 
+ print(List1[-3])
+ 
+ print(List[2])
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+        
